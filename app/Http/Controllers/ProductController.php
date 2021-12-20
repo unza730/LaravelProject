c27635926a6de4fd4653bf21cf7373ebe41e4bf7
unza730122 <sarisha505@gmail.com>
2021-12-21 01:11:36 +0500

    First Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+     $students = DB::table('product')->get();
+     return view('home', ['students'=>$students]);
+    
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+         DB::table('product')->insert([
+          'title' => $request->title,
+          'description' => $request->description,
+          'price' => $request->price,
+        ]);
+        return redirect(route('index'))->with('status','User Added!');
+       
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+         $students = DB::table('product')->find($id);
+        return view('editform', ['students'=>$students]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         $students = DB::table('product')->where('id', $id)->update([
+             'title' => $request->title,
+          'description' => $request->description,
+          'price' => $request->price,
+        ]);
+         return redirect(route('index'))->with('status','User Updated!');
+      
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table('product')->where('id', $id)->delete();
+         return redirect(route('index'))->with('status','User Deleted!');
+      
+    }
+}
